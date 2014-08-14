22ee4597366907005aa7ad4203d12ef1cc95985c
Shou Ya <shouyatf@gmail.com>
2014-08-15 03:53:05 +0800

    parsed instruction macros

--- assembler/Assembler.hs
+++ assembler/Assembler.hs
@@ -1,4 +1,4 @@
-module Assembler () where
+module Assembler where
 
 {-
 
@@ -6,34 +6,143 @@ A 8080 CPU assembler.
 
 -}
 
-import Data.Char (toUpper, toLower)
-import Data.Word
-import Data.Bits
+-- import Data.Char (toUpper, toLower)
+import Data.List (intercalate)
+-- import Data.Word
+-- import Data.Bits
+
+import Text.Printf
 
 import Control.Monad (liftM)
-import Control.Applicative ((*>), (<*))
+-- import Control.Applicative ((*>), (<*))
 
 import Text.ParserCombinators.Parsec
 import qualified Data.ByteString as BS
 
 
-data Address = ALbl String
-             | AHex Word16
-             | ALblOffset String Int
-             | AHexOffset Word16 Int
 
-data ParseResult = Label { llabel :: String }
-                 | Plain { ibyteCode :: BS.ByteString }
-                 | Goto  { gbyteCode :: BS.ByteString
-                         , goffset :: Offset }
+{- Instruction Specification List Parsing
+
+   instruction list can be generated with the tool below
+   `utils/genInstList.hs` from a opcode html file.
+-}
+
+data Register = A | B | C | D | E | H | L | M | PSW | SP | IP
+
+instance Show Register where
+  show A = "A"
+  show B = "B"
+  show C = "C"
+  show D = "D"
+  show E = "E"
+  show H = "H"
+  show L = "L"
+  show M = "[HL]"
+  show PSW = "PSW"
+  show SP = "SP"
+  show IP = "IP"
+
+data Parameter = Reg Register
+               | Addr
+               | Byte
+               | Word
+               | Parm Int
+
+instance Show Parameter where
+  show (Reg r)  = show r
+  show Addr     = "[addr]"
+  show Byte     = "xx"
+  show Word     = "xxxx"
+  show (Parm i) = show i
+
+data Instruction = Inst { _instName :: String
+                        , _instCode :: BS.ByteString
+                        , _instParams :: [Parameter]
+                        }
+
+instance Show Instruction where
+  show Inst { _instName = n
+            , _instCode = c
+            , _instParams = p } =
+    intercalate " " [showcode c, n, intercalate "," $ map show p]
+    where showcode = concatMap (printf "%02x") . BS.unpack
 
 
+
+instLen :: Instruction -> Integer
+instLen (Inst { _instCode = c
+              , _instParams = ps }) = fromIntegral (BS.length c) +
+                                      sum (map paramLen ps)
+
+paramLen :: Parameter -> Integer
+paramLen Reg {}  = 0
+paramLen Addr {} = 2
+paramLen Byte {} = 1
+paramLen Word {} = 2
+paramLen Parm {} = 0
+
+
+parseInstList :: String -> [Instruction]
+parseInstList = concatMap parseline . lines
+  where parseline str = case parse parseInstLine "" str of
+          Left _  -> []  -- error 'parse failed'
+          Right x -> [x]
+
+parseInstLine :: Parser Instruction
+parseInstLine = do code <- parseInstCode
+                   _ <- space
+                   name <- parseInstName
+                   _ <- optional space
+                   parm <- parseInstParams
+                   return $ Inst { _instCode = code
+                                 , _instName = name
+                                 , _instParams = parm }
+
+parseInstCode :: Parser (BS.ByteString)
+parseInstCode = liftM (BS.singleton . read . ("0x"++)) (count 2 hexDigit)
+
+parseInstName :: Parser String
+parseInstName = many1 upper
+
+parseInstParams :: Parser [Parameter]
+parseInstParams = parseInstParam `sepBy` char ','
+
+parseInstParam :: Parser Parameter
+parseInstParam =  (char 'A' >> return (Reg A))
+              <|> (char 'B' >> return (Reg B))
+              <|> (char 'C' >> return (Reg C))
+              <|> (char 'D' >> return (Reg D))
+              <|> (char 'E' >> return (Reg E))
+              <|> (char 'H' >> return (Reg H))
+              <|> (char 'L' >> return (Reg L))
+              <|> (char 'M' >> return (Reg M))  -- M == [HL]
+              <|> (string "PSW" >> return (Reg PSW))
+              <|> (string "SP" >> return (Reg SP))
+              <|> (string "IP" >> return (Reg IP))
+              <|> (string "word" >> return Word)
+              <|> (string "address" >> return Addr)
+              <|> (string "byte" >> return Byte)
+              <|> (many digit >>= return . Parm . read)
+
+
+
+
+{- End of instruction list parsing procedures -}
+
+{-
+data Register = A | B | C | D | E | H | L | M | PSW | SP | IP
+
+data Parameter = Reg Register
+               | Addr
+               | Byte
+               | Word
+               | Parm Int
+-}
+{-
 comp8x2 :: Word8 -> Word8 -> Word16
 comp8x2 a b = ((fromEnum a) `shiftL` 8) .|. (fromEnum b)
 
--- Case insensitive
-ciChar c = char (toLower c) <|> char (toUpper c)
-ciStr  s = try (mapM ciChar s)
+
 
 parseLine :: Parser [ParseResult]
 parseLine = do
@@ -45,18 +154,6 @@ parseLine = do
   optional parseComment
   skipMany space
   return (lbl ++ inst)
-
-
--- Shortcuts for registers
-A = string "A"
-B = string "B"
-C = string "C"
-D = string "D"
-E = string "E"
-H = string "H"
-L = string "L"
-HL = string "[HL]"
-
 
 
 {-
@@ -163,3 +260,4 @@ loadInst = readFile "instructions.macro" >>= return . filter foo . unlines
 
 
 parseFile = liftM join $ (many (parseLine >> optional newline)) <* eof
+-}
